1a66c975fe1e6b82759cafe0be5e3fa9fd71d040
Engineeer-ravi <pathakravi.er@gmail.com>
2025-01-30 12:29:43 +0530

    code

---
+++ Operator/EvenOdd.java
@@ -0,0 +1,23 @@
+public class EvenOdd{
+    public static void main(String []args){
+        // int a = 4;
+        // if(a%2==0){
+        //     System.out.println("Even");
+        // }
+        // else{
+        //     System.out.println("Odd");
+        // }
+        String color="green";
+        if(color=="red"){
+            System.out.println("Stop!");
+        }
+        if(color=="Yellow"){
+            System.out.println("Slow! Dowen");
+            
+        }
+        if(color=="green"){
+            System.out.println("Lets go");
+        }
+        
+    }
+}
